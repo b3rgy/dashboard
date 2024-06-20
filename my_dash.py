08dd2021f81920dac93f0334a12d6dd30ef13c39
b3rgy <spangenberg.marvin@gmail.com>
2024-06-20 10:34:52 +0200

    added server definition

--- my_dash.py
+++ my_dash.py
@@ -1,9 +1,16 @@
 import dash
 from dash import Dash, html
+import plotly
+import plotly.express as px
+import pandas as pd
+
 
 app =dash.Dash()
-
 app.layout = html.Div(html.H1(children = 'My First Spicy Dash'))
 
 if __name__ == '__main__':
      app.run_server() 
+
+
+
+server= app.server
