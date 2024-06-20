2dee7936653ddb1f7cddd4938625b39dd293bfd6
b3rgy <spangenberg.marvin@gmail.com>
2024-06-20 10:40:55 +0200

    committed the my_dash.py from the encounter notes

--- my_dash.py
+++ my_dash.py
@@ -1,16 +1,29 @@
 import dash
-from dash import Dash, html
-import plotly
-import plotly.express as px
-import pandas as pd
+from dash import html
 
+# 1. import the packages
+# 2. instantiate/load object
 
-app =dash.Dash()
+app = dash.Dash()  # creating our app, the variable holds an empty dashboard
+
 app.layout = html.Div(html.H1(children = 'My First Spicy Dash'))
 
+meep = 'Hello guys'
+
+
 if __name__ == '__main__':
-     app.run_server() 
+     app.run_server()
 
 
+# my_dash.py
+#  |
+#  ---> meep
 
-server= app.server
+#  my_test.py
+#  |
+#  ---> import my_dash
+#  ---> my_dash.meep
+
+#  Terminal: ```python my_dash.py```  -> __name__ = '__main__'
+#  Import:      import my_dash        -> __name__ != '__main__'
+
